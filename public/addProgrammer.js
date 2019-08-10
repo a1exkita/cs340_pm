e33c865ec05892fadbb1b144e92a93c12674776c
Atsuhito Kita <kitaa@oregonstate.edu>
2019-08-10 02:47:43 -0700

    done with the insert function finally

--- public/addProgrammer.js
+++ public/addProgrammer.js
@@ -3,33 +3,34 @@ var acceptBtn = document.getElementsByClassName('accept-button')[0];
 acceptBtn.addEventListener('click', function () {
 
    var selectProject = document.getElementById('select-project').value.trim();
-   var addDepartment = document.getElementById('add-department').value.trim();
+   var addProgrammer = document.getElementById('add-programmer').value.trim();
 
-   if(selectProject.length > 0 && addDepartment.length > 0){
+   if(selectProject.length > 0 && addProgrammer.length > 0){ 
       console.log(selectProject);
-      console.log(addDepartment);
-   }
+      console.log(addProgrammer);
+   }   
    else{
       alert('Select options in all required fields');
-   }
+   }   
    var postRequest = new XMLHttpRequest();
-   var requestURL = 'addDepartment';
+   var requestURL = 'addProgrammer';
    postRequest.open('POST', requestURL);
 
    var requestBody = JSON.stringify({
        selectProject: selectProject,
-       addDepartment: addDepartment
-   });
+       addProgrammer: addProgrammer
+   }); 
+
    postRequest.setRequestHeader('Content-Type', 'application/json');
    postRequest.send(requestBody);
-   window.location.pathname ='/addProgrammer';
+   window.location.pathname ='/';
 
    cleanInput();
 });
 
 function cleanInput() {
-    document.getElementById('select-project').value= "";
-    document.getElementById('add-department').value= "";
+    document.getElementById('select-project').value= ""; 
+    document.getElementById('add-programmer').value= ""; 
 }
 
 cleanInput();
--- public/insert.js
+++ public/insert.js
@@ -7,21 +7,14 @@ acceptBtn.addEventListener('click', function () {
    var addEnd = document.getElementById('end-date-attribution-input').value.trim();
    var addBudget = document.getElementById('budget-attribution-input').value.trim();
    var addClientId = document.getElementById('clientId-attribution-input').value.trim();
-   var addDepartmentId = document.getElementById('departmentId-attribution-input').value.trim();
-   var addProgrammerId = document.getElementById('programmerId-attribution-input').value.trim();
 
-   // if(addName.length > 0 && addStart.length > 0 && addEnd.length > 0 &&
-   //   addBudget.length > 0 && addClientId.length >= -1  && addDepartmentId.length >= -1 && addProgrammerId.length >= -1){
    if(addName.length > 0 && addStart.length > 0 && addEnd.length > 0 &&
      addBudget.length > 0 && addClientId.length >= -1 ){
-      //insertNewTwit(addTxt, addAuthor);//line 79 and 78 can go in here, and then can do on lines 85/86 addTxt.value = "";
       console.log(addName);
       console.log(addStart);
       console.log(addEnd);
       console.log(addBudget);
       console.log(addClientId);
-      // console.log(addDepartmentId);
-      // console.log(addProgrammerId);
    }
    else{
       alert('Enter values in all required fields');
@@ -36,8 +29,6 @@ acceptBtn.addEventListener('click', function () {
        endDate: addEnd,
        budget: addBudget,
        clientId: addClientId
-       // departmentId: addDepartmentId,
-       // programmerId: addProgrammerId
    });
 
    postRequest.setRequestHeader('Content-Type', 'application/json');
@@ -53,8 +44,6 @@ function cleanInput() {
   document.getElementById('end-date-attribution-input').value = "";
   document.getElementById('budget-attribution-input').value = "";
   document.getElementById('clientId-attribution-input').value = "";
-  document.getElementById('departmentId-attribution-input').value = "";
-  document.getElementById('programmerId-attribution-input').value = "";
 }
 
 cleanInput();
