4d0ae79f970f091d8beba006013748f457a22df3
Min Chew <chewm@flip2.engr.oregonstate.edu>
2019-08-11 17:27:19 -0700

    new package.json

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_kitaa',
-  password        : '1942',
-  database        : 'cs340_kitaa'
+  user            : '',
+  password        : '',
+  database        : ''
 });
 
 module.exports.pool = pool;
